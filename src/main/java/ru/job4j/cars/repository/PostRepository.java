94e7b71ec6697b773fd3c4bfe8184a72162d131a
yungdanie <nikoches253@gmail.com>
2022-10-04 11:20:30 +0300

    getLastDay post

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -5,6 +5,10 @@ import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 import java.util.function.Consumer;
 
 @AllArgsConstructor
@@ -16,5 +20,16 @@ public class PostRepository {
     public Post save(Post post) {
         repository.tx((Consumer<Session>) session -> session.persist(post));
         return post;
+    }
+
+    public List<Post> getLastDay() {
+        return repository.tx(session -> {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Post> criteriaQuery = builder.createQuery(Post.class);
+            Root<Post> root = criteriaQuery.from(Post.class);
+            return session.createQuery(criteriaQuery.select(root).
+                    where(builder.equal(criteriaQuery.subquery(Post.class).
+                            where(builder.greatest(root.get("created"))), root.get("created")))).list();
+        });
     }
 }
